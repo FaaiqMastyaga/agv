74445fafa93cc28bac2fd7b296a81f691c395dd2
FaaiqMastyaga <faaiqmastyaga@gmail.com>
2025-10-01 10:56:08 +0700

    launch(feat): create general launch file for aruco_localization

---
+++ agv_ws/src/agv_launch/launch/aruco_localization.launch.py
@@ -0,0 +1,30 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    agv_launch_dir = get_package_share_directory('agv_launch')
+    vision_dir = get_package_share_directory('vision')
+    localization_dir = get_package_share_directory('localization')
+
+    return LaunchDescription([
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(agv_launch_dir, 'launch', 'web_communication.launch.py')
+            )
+        ),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(vision_dir, 'launch', 'detect_aruco.launch.py')
+            )
+        ),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(localization_dir, 'launch', 'aruco_localization.launch.py')
+            )
+        ),
+    ])
