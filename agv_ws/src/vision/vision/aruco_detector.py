ec7e3dcb93f1e66a29796e3684964f5a89a80973
FaaiqMastyaga <faaiqmastyaga@gmail.com>
2025-10-19 20:34:07 +0700

    vision(feat): implement camera parameter updater

--- agv_ws/src/vision/vision/aruco_detector.py
+++ agv_ws/src/vision/vision/aruco_detector.py
@@ -1,13 +1,16 @@
 import rclpy
 from rclpy.node import Node
+from std_msgs.msg import String
 from sensor_msgs.msg import Image, CameraInfo
 from geometry_msgs.msg import Pose
 from aruco_msgs.msg import Marker, MarkerArray
+from map_database_interfaces.srv import GetMap
 from cv_bridge import CvBridge
 import cv2
 import numpy as np
 import tf_transformations
 import message_filters
+import threading
 
 class ArucoDetector(Node):
     ARUCO_DICT = {
@@ -33,16 +36,10 @@ class ArucoDetector(Node):
     def __init__(self):
         super().__init__('aruco_detector')
 
-        self.declare_parameters(
-            namespace='',
-            parameters=[
-                ('aruco_dict', rclpy.Parameter.Type.STRING),
-                ('marker_size', rclpy.Parameter.Type.DOUBLE)
-            ]
-        )
+        self.config_lock = threading.Lock()
 
-        self.aruco_dict_name = self.get_parameter('aruco_dict').get_parameter_value().string_value
-        self.marker_size = self.get_parameter('marker_size').get_parameter_value().double_value
+        self.aruco_dict_name = 'DICT_4X4_50'
+        self.marker_size = 8.0 # cm
 
         self.aruco_dict = cv2.aruco.Dictionary_get(self.ARUCO_DICT[self.aruco_dict_name])
         self.aruco_parameters = cv2.aruco.DetectorParameters_create()
@@ -56,6 +53,10 @@ class ArucoDetector(Node):
         # Subscriptions
         image_sub = message_filters.Subscriber(self, Image, '/camera/image_raw')
         camera_info_sub = message_filters.Subscriber(self, CameraInfo, '/camera/camera_info')
+        self.map_signal_sub_ = self.create_subscription(String, '/map_database/map_update_signal', self.mapSignalCallback, 10)
+
+        # Service Client
+        self.map_loader_client_ = self.create_client(GetMap, '/map_database/get_map')
 
         # Time synchronizer
         self.ts = message_filters.TimeSynchronizer([image_sub, camera_info_sub], 10)
@@ -66,23 +67,29 @@ class ArucoDetector(Node):
         self.image_marked_pub_ = self.create_publisher(Image, '/camera/image_marked', 10)
 
     def imageInfoCallback(self, image_msg, camera_info_msg):
-        # Update camera parameters from the CameraInfo message
-        self.camera_matrix = np.array(camera_info_msg.k).reshape(3,3)
-        self.distortion_coeffs = np.array(camera_info_msg.d)
+        with self.config_lock:
+            # Update camera parameters from the CameraInfo message
+            self.camera_matrix = np.array(camera_info_msg.k).reshape(3,3)
+            self.distortion_coeffs = np.array(camera_info_msg.d)
+
+            current_dict = self.aruco_dict
+            current_size = self.marker_size
+            current_cam_matrix = self.camera_matrix
+            current_dist_coeffs = self.distortion_coeffs
 
         # Process the image
         img = self.bridge.imgmsg_to_cv2(image_msg)
         aruco_marker_array = MarkerArray()
         aruco_marker_array.header = image_msg.header
 
-        (corners, ids, rejected) = cv2.aruco.detectMarkers(img, self.aruco_dict, parameters=self.aruco_parameters)
+        (corners, ids, rejected) = cv2.aruco.detectMarkers(img, current_dict, parameters=self.aruco_parameters)
 
         if len(corners) > 0:
             ids = ids.flatten()
 
             for (marker_corner, marker_id) in zip(corners, ids):
                 # Get marker pose (relative to camera)
-                rvec, tvec, _ = cv2.aruco.estimatePoseSingleMarkers(marker_corner, self.marker_size, self.camera_matrix, self.distortion_coeffs)
+                rvec, tvec, _ = cv2.aruco.estimatePoseSingleMarkers(marker_corner, current_size, current_cam_matrix, current_dist_coeffs)
                 rvec, tvec = rvec[0][0], tvec[0][0]
 
                 # Convert rotation vector to quaternion
@@ -121,6 +128,32 @@ class ArucoDetector(Node):
         img_msg = self.bridge.cv2_to_imgmsg(img, 'bgr8')
         img_msg.header = image_msg.header
         self.image_marked_pub_.publish(img_msg)
+
+    def mapSignalCallback(self, map_signal_msg):
+        self.loadMap()
+
+    def loadMap(self):
+        self.map_loader_req = GetMap.Request()
+        future = self.map_loader_client_.call_async(self.map_loader_req)
+        future.add_done_callback(self.updateMapParam)
+
+    def updateMapParam(self, future):
+        try:
+            result = future.result()
+        except Exception as e:
+            self.get_logger().error(f'Service call failed: {e}')
+        
+        if result.success:
+            new_dict_name = result.marker_dictionary
+            new_size = result.marker_size
+
+            with self.config_lock:
+                self.aruco_dict_name = new_dict_name
+                self.marker_size = new_size
+                self.aruco_dict = cv2.aruco.Dictionary_get(self.ARUCO_DICT[self.aruco_dict_name])
+                self.get_logger().info(f'Loaded map with dictionary: {self.aruco_dict_name}, size: {self.marker_size} cm.')
+        else:
+            self.get_logger().error('Map service reported failure.')
 
 def main(args=None):
     rclpy.init(args=args)
